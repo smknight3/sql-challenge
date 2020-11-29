1d8b439b77450b22617dede45eb9adedfb04e346
smknight3 <71661810+smknight3@users.noreply.github.com>
2020-11-29 11:36:13 -0500

    Add existing file

--- sql/table_schemas.sql
+++ sql/table_schemas.sql
@@ -1,52 +1,59 @@
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE employees(
+	emp_no int NOT NULL,
+	emp_title_id VARCHAR(30) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(3) NOT NULL,
+	hire_date DATE,
+	PRIMARY KEY(emp_no)
+	);
+	
 DROP TABLE IF EXISTS departments;
 
 CREATE TABLE departments(
-	id serial primary key,
-	dept_no varchar(30),
-	dept_name varchar(30)
+	dept_no varchar(30) NOT NULL,
+	dept_name varchar(30) NOT NULL, 
+	Primary Key (dept_no)
 	);
 
 DROP TABLE IF EXISTS dept_emp;
 
 CREATE TABLE dept_emp(
-	id serial primary key,
-	emp_no int,
-	dept_no varchar(30)
+	emp_no int NOT NULL,
+	dept_no varchar(30) NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 	);
 
 DROP TABLE IF EXISTS dept_manager;
 
 CREATE TABLE dept_manager(
-	id serial primary key,
-	dept_no varchar(30),
-	emp_no int
-	);
+	dept_no varchar(30) NOT NULL,
+	emp_no int NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no), 
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+		);
 
-DROP TABLE IF EXISTS employees;
-
-CREATE TABLE employees(
-	id serial primary key,
-	emp_no int,
-	emp_title_id VARCHAR(30),
-	birth_date DATE,
-	first_name VARCHAR(30),
-	last_name VARCHAR(30),
-	sex VARCHAR(3),
-	hire_date DATE
-	);
-
-DROP TAblE IF EXISTS salaries;
+DROP TABLE IF EXISTS salaries;
 
 CREATE TABLE salaries(
-	id serial primary key,
-	emp_no int, 
-	salary int
-	);
+	emp_no int NOT NULL, 
+	salary int NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
 
 DROP TABLE IF EXISTS titles;
 
 CREATE TABLE titles(
-	id serial primary key, 
-	title_id varchar(30),
-	title varchar(30)
+	title_id varchar(30) NOT NULL,
+	title varchar(30) NOT NULL,
+	PRIMARY KEY (title_id)
 	);
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY ("emp_title_id") REFERENCES "titles"(title_id);
